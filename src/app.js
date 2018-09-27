0b57c7d87b4e056cb716895f20a2d22450887be7
linh <jerrydotlam@gmail.com>
2018-09-27 11:37:41 +0800

    add tab icon

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import promisify from './plugins/promisify';
 
-// app.js
 App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
@@ -8,37 +7,12 @@ App({
    */
   onLaunch: function () {
     promisify();
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || [];
-    logs.unshift(Date.now());
-    wx.setStorageSync('logs', logs);
 
-    // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // });
-    // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo;
-
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res);
-    //           }
-    //         }
-    //       });
-    //     }
-    //   }
-    // });
+    const user = wx.getStorageSync('user');
+    if (user) {
+      this.globalData.userInfo = user;
+      this.globalData.accessToken = user.accessToken;
+    }
   },
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
@@ -46,7 +20,6 @@ App({
    */
   onShow: function () {
     console.log('app show');
-    // this.api();
   },
   /**
    * 当小程序从前台进入后台，会触发 onHide
@@ -72,27 +45,6 @@ App({
   onPageNotFound: function () {
     console.log('app page not found');
   },
-  /**
-   * 可以添加任意的函数或数据到 Object 参数中，用`this`可以访问
-   */
-  // api: function (url, data) {
-  //   const promise = new Promise((resolve, reject) => {
-  //     wx.request({
-  //       url: url,
-  //       data: data,
-  //       header: {},
-  //       method: 'POST',
-  //       dataType: 'json',
-  //       success: (res) => {
-  //         resolve(res);
-  //       },
-  //       fail: (err) => {
-  //         reject(err);
-  //       }
-  //     });
-  //   });
-  //   return promise;
-  // },
   globalData: {
     accessToken: '',
     userInfo: null
